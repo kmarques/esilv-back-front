5538fe5e7f90e9c2368f6c77481ce41c758b5e02
Karl MARQUES <karl@kmarques.dev>
2025-09-23 09:04:51 +0200

    BIC1: added nodemon package

--- BIC1/backend/server.js
+++ BIC1/backend/server.js
@@ -3,5 +3,5 @@ const express = require("express");
 const app = express();
 
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log("Server listening on port http://localhost:3000");
 });
